22ec6401635be2535bc88ae24df2041fdc88f663
Joerg <joerg.wood@gmail.com>
2020-05-18 17:21:12 +1000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.EntryBox = New System.Windows.Forms.TextBox()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.Bdot = New System.Windows.Forms.Button()
+        Me.B0 = New System.Windows.Forms.Button()
+        Me.Btimes = New System.Windows.Forms.Button()
+        Me.Bdivide = New System.Windows.Forms.Button()
+        Me.Bminus = New System.Windows.Forms.Button()
+        Me.Bplus = New System.Windows.Forms.Button()
+        Me.Benter = New System.Windows.Forms.Button()
+        Me.BC = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'B1
+        '
+        Me.B1.Location = New System.Drawing.Point(12, 226)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(50, 41)
+        Me.B1.TabIndex = 0
+        Me.B1.TabStop = False
+        Me.B1.Text = "1"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'EntryBox
+        '
+        Me.EntryBox.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.EntryBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EntryBox.Location = New System.Drawing.Point(12, 12)
+        Me.EntryBox.Name = "EntryBox"
+        Me.EntryBox.ReadOnly = True
+        Me.EntryBox.Size = New System.Drawing.Size(218, 41)
+        Me.EntryBox.TabIndex = 5
+        Me.EntryBox.TabStop = False
+        '
+        'B2
+        '
+        Me.B2.Location = New System.Drawing.Point(68, 226)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(50, 41)
+        Me.B2.TabIndex = 6
+        Me.B2.TabStop = False
+        Me.B2.Text = "2"
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'B3
+        '
+        Me.B3.Location = New System.Drawing.Point(124, 226)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(50, 41)
+        Me.B3.TabIndex = 7
+        Me.B3.TabStop = False
+        Me.B3.Text = "3"
+        Me.B3.UseVisualStyleBackColor = True
+        '
+        'B6
+        '
+        Me.B6.Location = New System.Drawing.Point(124, 179)
+        Me.B6.Name = "B6"
+        Me.B6.Size = New System.Drawing.Size(50, 41)
+        Me.B6.TabIndex = 10
+        Me.B6.TabStop = False
+        Me.B6.Text = "6"
+        Me.B6.UseVisualStyleBackColor = True
+        '
+        'B5
+        '
+        Me.B5.Location = New System.Drawing.Point(68, 179)
+        Me.B5.Name = "B5"
+        Me.B5.Size = New System.Drawing.Size(50, 41)
+        Me.B5.TabIndex = 9
+        Me.B5.TabStop = False
+        Me.B5.Text = "5"
+        Me.B5.UseVisualStyleBackColor = True
+        '
+        'B4
+        '
+        Me.B4.Location = New System.Drawing.Point(12, 179)
+        Me.B4.Name = "B4"
+        Me.B4.Size = New System.Drawing.Size(50, 41)
+        Me.B4.TabIndex = 8
+        Me.B4.TabStop = False
+        Me.B4.Text = "4"
+        Me.B4.UseVisualStyleBackColor = True
+        '
+        'B9
+        '
+        Me.B9.Location = New System.Drawing.Point(124, 132)
+        Me.B9.Name = "B9"
+        Me.B9.Size = New System.Drawing.Size(50, 41)
+        Me.B9.TabIndex = 13
+        Me.B9.TabStop = False
+        Me.B9.Text = "9"
+        Me.B9.UseVisualStyleBackColor = True
+        '
+        'B8
+        '
+        Me.B8.Location = New System.Drawing.Point(68, 132)
+        Me.B8.Name = "B8"
+        Me.B8.Size = New System.Drawing.Size(50, 41)
+        Me.B8.TabIndex = 12
+        Me.B8.TabStop = False
+        Me.B8.Text = "8"
+        Me.B8.UseVisualStyleBackColor = True
+        '
+        'B7
+        '
+        Me.B7.Location = New System.Drawing.Point(12, 132)
+        Me.B7.Name = "B7"
+        Me.B7.Size = New System.Drawing.Size(50, 41)
+        Me.B7.TabIndex = 11
+        Me.B7.TabStop = False
+        Me.B7.Text = "7"
+        Me.B7.UseVisualStyleBackColor = True
+        '
+        'Bdot
+        '
+        Me.Bdot.Location = New System.Drawing.Point(124, 273)
+        Me.Bdot.Name = "Bdot"
+        Me.Bdot.Size = New System.Drawing.Size(50, 41)
+        Me.Bdot.TabIndex = 15
+        Me.Bdot.TabStop = False
+        Me.Bdot.Text = "."
+        Me.Bdot.UseVisualStyleBackColor = True
+        '
+        'B0
+        '
+        Me.B0.Location = New System.Drawing.Point(12, 273)
+        Me.B0.Name = "B0"
+        Me.B0.Size = New System.Drawing.Size(106, 41)
+        Me.B0.TabIndex = 14
+        Me.B0.TabStop = False
+        Me.B0.Text = "0"
+        Me.B0.UseVisualStyleBackColor = True
+        '
+        'Btimes
+        '
+        Me.Btimes.Location = New System.Drawing.Point(124, 85)
+        Me.Btimes.Name = "Btimes"
+        Me.Btimes.Size = New System.Drawing.Size(50, 41)
+        Me.Btimes.TabIndex = 17
+        Me.Btimes.TabStop = False
+        Me.Btimes.Text = "X"
+        Me.Btimes.UseVisualStyleBackColor = True
+        '
+        'Bdivide
+        '
+        Me.Bdivide.Location = New System.Drawing.Point(68, 85)
+        Me.Bdivide.Name = "Bdivide"
+        Me.Bdivide.Size = New System.Drawing.Size(50, 41)
+        Me.Bdivide.TabIndex = 16
+        Me.Bdivide.TabStop = False
+        Me.Bdivide.Text = "/"
+        Me.Bdivide.UseVisualStyleBackColor = True
+        '
+        'Bminus
+        '
+        Me.Bminus.Location = New System.Drawing.Point(180, 85)
+        Me.Bminus.Name = "Bminus"
+        Me.Bminus.Size = New System.Drawing.Size(50, 41)
+        Me.Bminus.TabIndex = 20
+        Me.Bminus.TabStop = False
+        Me.Bminus.Text = "-"
+        Me.Bminus.UseVisualStyleBackColor = True
+        '
+        'Bplus
+        '
+        Me.Bplus.Location = New System.Drawing.Point(180, 132)
+        Me.Bplus.Name = "Bplus"
+        Me.Bplus.Size = New System.Drawing.Size(50, 88)
+        Me.Bplus.TabIndex = 19
+        Me.Bplus.TabStop = False
+        Me.Bplus.Text = "+"
+        Me.Bplus.UseVisualStyleBackColor = True
+        '
+        'Benter
+        '
+        Me.Benter.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Benter.Location = New System.Drawing.Point(180, 226)
+        Me.Benter.Name = "Benter"
+        Me.Benter.Size = New System.Drawing.Size(50, 88)
+        Me.Benter.TabIndex = 18
+        Me.Benter.TabStop = False
+        Me.Benter.Text = "="
+        Me.Benter.UseVisualStyleBackColor = True
+        '
+        'BC
+        '
+        Me.BC.Location = New System.Drawing.Point(12, 85)
+        Me.BC.Name = "BC"
+        Me.BC.Size = New System.Drawing.Size(49, 40)
+        Me.BC.TabIndex = 21
+        Me.BC.TabStop = False
+        Me.BC.Text = "C"
+        Me.BC.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(250, 337)
+        Me.Controls.Add(Me.BC)
+        Me.Controls.Add(Me.Bminus)
+        Me.Controls.Add(Me.Bplus)
+        Me.Controls.Add(Me.Benter)
+        Me.Controls.Add(Me.Btimes)
+        Me.Controls.Add(Me.Bdivide)
+        Me.Controls.Add(Me.Bdot)
+        Me.Controls.Add(Me.B0)
+        Me.Controls.Add(Me.B9)
+        Me.Controls.Add(Me.B8)
+        Me.Controls.Add(Me.B7)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.EntryBox)
+        Me.Controls.Add(Me.B1)
+        Me.Name = "Form1"
+        Me.Text = "PGC"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents B1 As Button
+    Friend WithEvents EntryBox As TextBox
+    Friend WithEvents B2 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents B6 As Button
+    Friend WithEvents B5 As Button
+    Friend WithEvents B4 As Button
+    Friend WithEvents B9 As Button
+    Friend WithEvents B8 As Button
+    Friend WithEvents B7 As Button
+    Friend WithEvents Bdot As Button
+    Friend WithEvents B0 As Button
+    Friend WithEvents Btimes As Button
+    Friend WithEvents Bdivide As Button
+    Friend WithEvents Bminus As Button
+    Friend WithEvents Bplus As Button
+    Friend WithEvents Benter As Button
+    Friend WithEvents BC As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,223 @@
+﻿Public Class Form1
+
+    Dim num1 As Double
+    Dim num2 As Double
+    Dim operation As String
+    Dim first As Boolean = True
+    Dim rDisplayed As Boolean = False
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub B1_Click(sender As Object, e As EventArgs) Handles B1.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "1"
+    End Sub
+
+    Private Sub B2_Click(sender As Object, e As EventArgs) Handles B2.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "2"
+    End Sub
+
+    Private Sub B3_Click(sender As Object, e As EventArgs) Handles B3.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "3"
+    End Sub
+
+    Private Sub B4_Click(sender As Object, e As EventArgs) Handles B4.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "4"
+    End Sub
+
+    Private Sub B5_Click(sender As Object, e As EventArgs) Handles B5.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "5"
+    End Sub
+
+    Private Sub B6_Click(sender As Object, e As EventArgs) Handles B6.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "6"
+    End Sub
+
+    Private Sub B7_Click(sender As Object, e As EventArgs) Handles B7.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "7"
+    End Sub
+
+    Private Sub B8_Click(sender As Object, e As EventArgs) Handles B8.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "8"
+    End Sub
+
+    Private Sub B9_Click(sender As Object, e As EventArgs) Handles B9.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "9"
+    End Sub
+    Private Sub B0_Click(sender As Object, e As EventArgs) Handles B9.Click
+        If rDisplayed = True Then
+            EntryBox.Text = ""
+            rDisplayed = False
+        End If
+        EntryBox.Text = EntryBox.Text & "0"
+    End Sub
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress, EntryBox.KeyPress, Btimes.KeyPress, Bplus.KeyPress, Bminus.KeyPress, Benter.KeyPress, Bdot.KeyPress, Bdivide.KeyPress, B9.KeyPress, B8.KeyPress, B7.KeyPress, B6.KeyPress, B5.KeyPress, B4.KeyPress, B3.KeyPress, B2.KeyPress, B1.KeyPress, B0.KeyPress, BC.KeyPress
+        '48 to 57 are the char codes for "0 to 9" of alphanumeric keypad. 
+        'If e.KeyChar = Chr(49) Then EntryBox.Text = EntryBox.Text & "1"
+        If IsNumeric(e.KeyChar) = True Then
+            If rDisplayed = True Then
+                EntryBox.Text = ""
+                rDisplayed = False
+            End If
+            EntryBox.Text = EntryBox.Text & e.KeyChar.ToString
+            Exit Sub
+        End If
+
+        If e.KeyChar = vbCr Then 'if enter pressed
+            If first = False Then
+                num2 = Val(EntryBox.Text)
+                Calculate()
+            End If
+        End If
+
+        If e.KeyChar = "." Then 'decimal pressed
+            If InStr(EntryBox.Text, ".") = 0 Then EntryBox.Text = EntryBox.Text & "."
+        End If
+
+        If e.KeyChar = "/" Or e.KeyChar = "*" Or e.KeyChar = "-" Or e.KeyChar = "+" Then
+            operation = e.KeyChar
+            first = False
+            num1 = Val(EntryBox.Text)
+            EntryBox.Text = ""
+        End If
+
+    End Sub
+
+    Private Sub Calculate()
+
+        Dim result As Double
+
+        If operation = "/" Then
+            result = num1 / num2
+        End If
+
+        If operation = "*" Then
+            result = num1 * num2
+        End If
+
+        If operation = "-" Then
+            result = num1 - num2
+        End If
+
+        If operation = "+" Then
+            result = num1 + num2
+        End If
+
+
+        If num1.ToString.Length > 2 And num2.ToString.Length > 2 Then
+
+            Dim ch1 As String = num1.ToString.Substring(num1.ToString.Length - 1)
+            Dim ch2 As String = num2.ToString.Substring(num2.ToString.Length - 1)
+
+            If (ch1 = 7 Or ch1 = 3) And (ch2 = 7 Or ch2 = 3) Then
+
+                If operation = "/" Or operation = "*" Then
+
+                    Dim rando As Integer = CInt(Int((10 * Rnd()) + 1))
+
+                    If rando = 5 Then
+                        result = result + Val(ch1)
+                    End If
+
+                    If rando = 7 Then
+                        result = result + Val(ch2)
+                    End If
+
+                End If
+            End If
+
+        End If
+
+        EntryBox.Text = result
+        rDisplayed = True
+
+    End Sub
+
+    Private Sub Bdot_Click(sender As Object, e As EventArgs) Handles Bdot.Click
+        If InStr(EntryBox.Text, ".") = 0 Then EntryBox.Text = EntryBox.Text & "."
+    End Sub
+
+    Private Sub BC_Click(sender As Object, e As EventArgs) Handles BC.Click
+        'clear
+        num1 = 0
+        num2 = 0
+        operation = ""
+        first = True
+        EntryBox.Text = ""
+
+    End Sub
+
+    Private Sub Benter_Click(sender As Object, e As EventArgs) Handles Benter.Click
+        If first = False Then
+            num2 = Val(EntryBox.Text)
+            Calculate()
+        End If
+    End Sub
+
+    Private Sub Bplus_Click(sender As Object, e As EventArgs) Handles Bplus.Click
+        operation = "+"
+        first = False
+        num1 = Val(EntryBox.Text)
+        EntryBox.Text = ""
+    End Sub
+
+    Private Sub Bminus_Click(sender As Object, e As EventArgs) Handles Bminus.Click
+        operation = "-"
+        first = False
+        num1 = Val(EntryBox.Text)
+        EntryBox.Text = ""
+    End Sub
+
+    Private Sub Btimes_Click(sender As Object, e As EventArgs) Handles Btimes.Click
+        operation = "*"
+        first = False
+        num1 = Val(EntryBox.Text)
+        EntryBox.Text = ""
+    End Sub
+
+    Private Sub Bdivide_Click(sender As Object, e As EventArgs) Handles Bdivide.Click
+        operation = "/"
+        first = False
+        num1 = Val(EntryBox.Text)
+        EntryBox.Text = ""
+    End Sub
+End Class
